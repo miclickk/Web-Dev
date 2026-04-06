804b7bcd9acc141cea9b0e75b0a7d3c3d7dad496
miclickk <askar050307@gmail.com>
2026-04-06 11:50:43 +0500

    Lab 9 (UPDATED LAB 8)

--- Lab8/api/admin.py
+++ Lab8/api/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "count", "is_active", "category")
+    list_filter = ("is_active", "category")
+    search_fields = ("name",)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
